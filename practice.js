f2be2d387f4447ee281f43b64be745e69d87a50a
sesameng <sesameng9@gmail.com>
2023-02-27 18:55:14 -0500

    solve majority of practice.js

--- practice.js
+++ practice.js
@@ -25,12 +25,24 @@ var moreFruits = function (fruits) {
 // use _.each to traverse the number array and determine
 // which are multiples of five.
 var multiplesOfFive = function (numbers) {
-
+  var fives = 0;
+  _.each(numbers, function(fruit, index, collection) {
+    if (fruit % 5 === 0) {
+      fives++;
+    }
+  });
+  return fives;
 };
 
 // use _.each to build an array containing only tweets belonging to a specified user.
 var getUserTweets = function(tweets, user) {
-
+  var userTweets = [];
+  _.each(tweets, function(tweet, index, tweets) {
+    if (tweet.user === user) {
+      userTweets.push(tweet);
+    }
+  });
+  return userTweets;
 };
 
 /*
@@ -41,23 +53,31 @@ var getUserTweets = function(tweets, user) {
 
 // use _.filter to return the fruits array with only the desired fruit.
 var onlyOneFruit = function (fruits, targetFruit) {
-
+  return _.filter(fruits, function(fruit, index, fruits) {
+    return targetFruit === fruit;
+  });
 };
 
 // use _.filter to return the fruits array with only fruits
 // starting with the letter 'P'.
 var startsWith = function (fruits, letter) {
-
+  return _.filter(fruits, function(fruit, index, fruits) {
+    return fruit[0] === letter;
+  });
 };
 
 // return a filtered array containing only cookie-type desserts.
 var cookiesOnly = function (desserts) {
-
+  return _.filter(desserts, function(dessert, index, desserts) {
+    return dessert.type === 'cookie';
+  });
 };
 
 // rebuild the getUserTweets function from above with _.filter instead
 var filterUserTweets = function(tweets, user) {
-
+  return _.filter(tweets, function(tweet, index, tweets) {
+    return tweet.user === user;
+  });
 };
 
 /*
@@ -69,20 +89,29 @@ var filterUserTweets = function(tweets, user) {
 // given an array of strings, use _.map to return a new array containing all
 // strings converted to uppercase letters.
 var upperCaseFruits = function (fruits) {
-
+  return _.map(fruits, function(fruit, index, fruits) {
+    return fruit.toUpperCase();
+  });
 };
 
 // given an array of dessert objects, return a new array of objects
 // that have a new "glutenFree" property, with a boolean value.
 // TIP: Items that contain flour are not gluten-free.
 var glutenFree = function (desserts) {
-
+  return _.map(desserts, function(dessert, index, desserts) {
+    var ingredients = dessert.ingredients;
+    dessert.glutenFree = ingredients.indexOf('flour') === -1;
+    return dessert;
+  });
 };
 
 // given an array of tweet objects, return a new array of strings
 // containing only the message properties.
 var allUserMessages = function(tweets) {
-
+  return _.map(tweets, function(tweet, index, tweets) {
+    var message = tweet.message;
+    return message;
+  });
 };
 
 // use _.map to return an array of items with their sale prices, with a new property
@@ -106,7 +135,17 @@ var allUserMessages = function(tweets) {
 
 */
 var applyCoupon = function (groceries, coupon) {
-
+  var applyCoupon = function (item) {
+    var itemPrice = item.price;
+    var itemPriceDigits = itemPrice.slice(1, itemPrice.length);
+    var itemPriceNum = (Number(itemPriceDigits)) * 100;
+    var couponNum = coupon * 100;
+    var saleAmount = itemPriceNum * coupon;
+    var salePrice = Math.round(itemPriceNum - saleAmount) / 100;
+    item.salePrice = '$' + salePrice;
+    return item;
+  };
+  return _.map(groceries, applyCoupon);
 };
 
 /*
@@ -117,13 +156,30 @@ var applyCoupon = function (groceries, coupon) {
 
 // return the total price of all products.
 var sumTotal = function (products) {
+  var getSum = function (accumulator, item) {
+    var itemPrice = item.price;
+    var itemPriceDigits = itemPrice.slice(1, itemPrice.length);
+    var itemPriceNum = (Number(itemPriceDigits));
+    accumulator = accumulator + itemPriceNum;
+    return accumulator;
+  };
+  return _.reduce(products, getSum, 0);
 
 };
 
 // return an object consisting of dessert types and how many of each.
 // exampleOutput: { dessertType: 3, dessertType2: 1 }
 var dessertCategories = function (desserts) {
-
+  var getDessertTypes = function (accumulator, dessert) {
+    var dessertType = dessert.type;
+    if (accumulator[dessertType] === undefined) {
+      accumulator[dessertType] = 1;
+    } else {
+      accumulator[dessertType] += 1;
+    }
+    return accumulator;
+  };
+  return _.reduce(desserts, getDessertTypes, {});
 };
 
 // return an object with the proper count of all user messages
